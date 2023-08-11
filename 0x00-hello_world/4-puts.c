7acf1338f5213f427835476a65d0e02e8feda0d3
Fideltodayy <fidelotieno11@gmail.com>
2023-08-10 18:49:43 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main - Printing a line with puts function
+ *
+ * Return : 0 (Success)
+ */
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+
+}
